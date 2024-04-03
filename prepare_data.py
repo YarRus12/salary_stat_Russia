cbdc84f9b02c3b8a1d73e9fb1385a70d6697d4d8
YarRus12 <yaroslav.russu@mail.ru>
2024-04-03 12:48:53 +0300

    Добавляем в проект обработку корреляции роста ВВП и роста ВВП/чел к росту ЗП

--- prepare_data.py
+++ prepare_data.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 attributes = {
     'nominal_salary': 'Средняя заработная плата',
-    'current_inflation': 'inflation_rate',
+    'current_inflation': 'Текущий уровень инфляции',
     'previous_year_inflation': 'Инфляция прошлого года',
     'real_salary': 'Реальный размер заработной платы',
     'real_salary_delta': 'Изменение реальной ЗП'
@@ -17,7 +17,7 @@ real_salary_delta = attributes['real_salary_delta']
 
 
 @st.cache_data
-def economy_actvity_data(choosen_activity):
+def economy_activity_data(chosen_activity):
     df_2016 = pd.read_excel("tab3-zpl_2023.xlsx", sheet_name='2000-2016 гг.', index_col=None)[
               2:]  # первые строки не несут информации для нас
     column_list = ['Вид деятельности', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009',
@@ -27,11 +27,11 @@ def economy_actvity_data(choosen_activity):
     df_2016.rename(columns=rename_dict, inplace=True)
 
     # Переименование старых колонок с похожими сферами деятельности
-    if 'деятельность гостиниц и предприятий общественного питания' in choosen_activity:
-        df_2016['Вид деятельности'] = df_2016['Вид деятельности']\
+    if 'деятельность гостиниц и предприятий общественного питания' in chosen_activity:
+        df_2016['Вид деятельности'] = df_2016['Вид деятельности'] \
             .replace('Операции с недвижимым имуществом, аренда и предоставление услуг',
                      'деятельность гостиниц и предприятий общественного питания')
-    if 'деятельность в области здравоохранения и социальных услуг' in choosen_activity:
+    if 'деятельность в области здравоохранения и социальных услуг' in chosen_activity:
         df_2016['Вид деятельности'] = df_2016['Вид деятельности'] \
             .replace('Здравоохранение и предоставление социальных услуг',
                      'деятельность в области здравоохранения и социальных услуг')
@@ -47,15 +47,17 @@ def economy_actvity_data(choosen_activity):
     df_2017.rename(columns=rename_dict, inplace=True)
     # Колонка "вид деятельности" очищается от регистрозависимости и пробелов
     df_2017['Вид деятельности'] = df_2017['Вид деятельности'].str.lower().str.strip()
-    merged = (pd.merge(df_2016[df_2016['Вид деятельности'].str.lower().str.strip().isin(choosen_activity)],
-                     df_2017[df_2017['Вид деятельности'].str.lower().str.strip().isin(choosen_activity)],
-                     on='Вид деятельности')
-            .melt(id_vars='Вид деятельности', var_name='year', value_name=nominal_salary)
-            )
+    # Объединяем df и преобразуем годы из столбцов в колонки для удобства соединения с инфляцией
+    merged = (pd.merge(df_2016[df_2016['Вид деятельности'].str.lower().str.strip().isin(chosen_activity)],
+                       df_2017[df_2017['Вид деятельности'].str.lower().str.strip().isin(chosen_activity)],
+                       on='Вид деятельности')
+              .melt(id_vars='Вид деятельности', var_name='year', value_name=nominal_salary)
+              )
     return merged
 
+
 @st.cache_data
-def infliation_data():
+def inflation_data():
     url = 'https://xn----ctbjnaatncev9av3a8f8b.xn--p1ai/%D1%82%D0%B0%D0%B1%D0%BB%D0%B8%D1%86%D1%8B-%D0%B8%D0%BD%D1%84%D0%BB%D1%8F%D1%86%D0%B8%D0%B8'
     r = requests.get(url)
     if r.status_code == 200:
@@ -72,6 +74,42 @@ def infliation_data():
     inflation[previous_year_inflation] = inflation[current_inflation].shift(1)
     return inflation
 
+
+def gross_domestic_product(file_name, result_column):
+    """
+    Работает для обработки как данных ВВП так и для обработки ВВП на чел
+    :param file_name:
+    :return:
+    """
+    vvp_url = f'https://rosstat.gov.ru/storage/mediabank/{file_name}'
+    wget_command = ['wget', '--limit-rate=100k', vvp_url]
+    subprocess.run(wget_command)
+
+    vvp_old = pd.read_excel(file_name, sheet_name='1', index_col=None)[1:].T
+    vvp_old.rename(columns={1: 'year', 2: 'ВВП в трлн руб.'}, inplace=True)
+    vvp_old['year'] = vvp_old['year'].astype(int)
+    vvp_old['ВВП в трлн руб.'] = vvp_old['ВВП в трлн руб.'].astype(float)
+    vvp_old.reset_index(drop=True, inplace=True)
+
+    vvp = pd.read_excel(file_name, sheet_name='2', index_col=None)[2:4].T
+    vvp.rename(columns={2: 'year', 3: 'ВВП в трлн руб.'}, inplace=True)
+    if result_column == 'Изменение ВВП на чел %':
+        vvp = pd.read_excel(file_name, sheet_name='2', index_col=None)[1:3].T
+        vvp.rename(columns={1: 'year', 2: 'ВВП в трлн руб.'}, inplace=True)
+
+    vvp = vvp[['year', 'ВВП в трлн руб.']].astype(str)
+    vvp['year'] = vvp['year'].str.slice(0, 4)
+    vvp['year'] = vvp['year'].astype(int)
+    vvp['ВВП в трлн руб.'] = vvp['ВВП в трлн руб.'].astype(float)
+    vvp.reset_index(drop=True, inplace=True)
+
+    merged_df = pd.merge(vvp_old, vvp, on=['year', 'ВВП в трлн руб.'], how='outer') \
+        .drop_duplicates(subset=['year'], keep='first')  # 2011 расчитывался по обеим методологиями, оставляю первый
+    merged_df[result_column] = (merged_df['ВВП в трлн руб.'] - merged_df['ВВП в трлн руб.'].shift(1)) / merged_df[
+        'ВВП в трлн руб.'] * 100
+    return merged_df
+
+
 @st.cache_data
 def main(choosen_activity):
     # удалим старый файл с данными
@@ -81,14 +119,44 @@ def main(choosen_activity):
     wget_command = ['wget', '--limit-rate=100k', stat_url]
     subprocess.run(wget_command)
     # готовим данные
-    economy_df = economy_actvity_data(choosen_activity)
-    inflation_df = infliation_data()
+    economy_df = economy_activity_data(choosen_activity)
+    inflation_df = inflation_data()
     result = economy_df.merge(inflation_df, on='year')
     result[real_salary] = result[nominal_salary] * (
-                (100 - result[previous_year_inflation]) / 100)
+            (100 - result[previous_year_inflation]) / 100)
     result[real_salary_delta] = result.groupby('Вид деятельности')[
-                                                       real_salary].pct_change() * 100
+                                    real_salary].pct_change() * 100
     return result[['year', 'Вид деятельности', real_salary,
                    previous_year_inflation, current_inflation, real_salary_delta]]
 
 
+def extra_metrics(extra):
+    result_extra_dict = {}
+    if 'ВВП' in extra:
+        col = 'Изменение ВВП %'
+        result_extra_dict['ВВП'] = {'dataframe': gross_domestic_product(
+            file_name="VVP_god_s_1995.xlsx",
+            result_column=col),
+            'column': col
+        }
+
+    if 'ВВП на душу населения' in extra:
+        col = 'Изменение ВВП на чел %'
+        result_extra_dict['ВВП на душу населения'] = {'dataframe': gross_domestic_product(
+            file_name="VVP_na_dushu_s_1995.xlsx",
+            result_column=col),
+            'column': col
+        }
+
+    if 'ВНП' in extra:
+        result_extra_dict['ВНП'] = None
+        pass
+
+    if 'Уровень счастья' in extra:
+        result_extra_dict['Уровень счастья'] = None
+        pass
+
+    if 'Уровень безработицы' in extra:
+        result_extra_dict['Уровень безработицы'] = None
+        pass
+    return result_extra_dict
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import altair as alt
-from prepare_data import main, previous_year_inflation, real_salary, real_salary_delta
+from prepare_data import main, extra_metrics, previous_year_inflation, real_salary, real_salary_delta
 import numpy as np
 
 
 CHOSEN_ACTIVITY = ['добыча полезных ископаемых', 'обрабатывающие производства', 'строительство',
                    'деятельность гостиниц и предприятий общественного питания', 'образование',
                    'деятельность в области здравоохранения и социальных услуг']
+EXTRA_METRICS = ['ВВП', 'ВВП на душу населения', 'ВВП на душу населения', 'ВНП', 'Уровень счастья',
+                 'Уровень безработицы']
 
 
 @st.cache_data(ttl=60 * 60 * 24)
-def create_schedule(dataframe):
+def create_schedule_main(dataframe):
 
     bar_chart = alt.Chart(dataframe).mark_bar().encode(
         x=alt.X('year:O', axis=alt.Axis(format='')),
@@ -50,7 +52,45 @@ def create_schedule(dataframe):
     st.altair_chart(combined_chart, use_container_width=True)
 
 
-def corr_coefficient(dataframe):
+def create_schedule_vvp(df_general, df_vvp, extra_column):
+
+    merged = df_general.merge(df_vvp, on='year')
+
+    bar_chart = alt.Chart(merged).mark_bar().encode(
+        x=alt.X('year:O', axis=alt.Axis(title='Год', labelAngle=0, tickMinStep=1)),  # Ось x - годы
+        y=alt.Y(f'{extra_column}:Q', axis=alt.Axis(title=extra_column, tickMinStep=5))  # Ось y - ВВП в трлн руб.
+    ).properties(
+        width=1000,  # Ширина графика
+        height=400  # Высота графика
+    )
+
+    # Создание линии % изменения реальной заработной платы в сравнении с предыдущим периодом
+    line_delta_salary = alt.Chart(df_general).mark_line(color='green').encode(
+        y=alt.Y(real_salary_delta, axis=alt.Axis(titleColor='white', labelColor='green')),
+        x=alt.Y('year:O', axis=alt.Axis(titleColor='white')),
+        size=alt.value(5)
+    ).properties(
+        width=1000,
+        height=400
+    )
+    combined_chart = alt.layer(bar_chart, line_delta_salary).resolve_scale(y='independent')
+    st.altair_chart(combined_chart, use_container_width=True)
+
+
+
+def corr_coefficient_extra(df_general, df_vvp, extra_column, general_column='изменением реальной заработной платы'):
+
+    filled_length = len(df_general.merge(df_vvp, on='year'))-2
+    delta_salary_list = df_general[real_salary_delta].tolist()[-filled_length:]  # Первое число всегда nan - его мы удаляем
+    extra_column_list = df_vvp[extra_column].tolist()[
+                                  -filled_length:]  # не длиннее delta зп
+
+    st.info(f"Коэффициент корреляции Пирсона между {extra_column} и {general_column} "
+            f"в сфере - {activity.capitalize()}: "
+            f"{str(np.corrcoef(extra_column_list, delta_salary_list)[0, 1])}")
+
+
+def corr_coefficient_main(dataframe):
     delta_salary_list = dataframe[real_salary_delta].tolist()[1:]  # Первое число всегда nan - его мы удаляем
     inflation_last_year_list = dataframe[previous_year_inflation].tolist()[
                                :len(delta_salary_list)]  # не длиннее delta зп
@@ -59,9 +99,6 @@ def corr_coefficient(dataframe):
     st.info(f"Коэффициент корреляции Пирсона между инфляцией прошлого года и изменением реальной заработной платы "
             f"в сфере - {activity.capitalize()}: "
             f"{str(np.corrcoef(inflation_last_year_list, delta_salary_list)[0, 1])}")
-    # inflation_curr_year_list = dataframe[current_inflation].tolist()[:len(delta_salary_list)]  # не длиннее delta зп
-    # st.info(f"Коэффициент корреляции Пирсона между инфляцией текущего года и изменением заработной платы: "
-    #         f"{str(np.corrcoef(inflation_curr_year_list, delta_salary_list)[0, 1])}")
 
 
 if __name__ == '__main__':
@@ -88,6 +125,11 @@ if __name__ == '__main__':
         max_value=max_value,
         value=[min_value, max_value])
     st.write("")
+
+    selected_extra = st.multiselect(
+        'Посмотрим дополнительные метрики для выбранных отраслей?',
+        EXTRA_METRICS, EXTRA_METRICS)
+
     filtered_df = res[
         (res['Вид деятельности'].isin(selected_activity))
         & (res['year'] <= to_year)
@@ -98,16 +140,48 @@ if __name__ == '__main__':
     st.write("")
 
     for activity in selected_activity:
+
+        st.title(activity.capitalize())
         # Создаем график для каждой сферы
         df = filtered_df[filtered_df['Вид деятельности'] == activity]
-        corr_coefficient(dataframe=df)
-        st.write("")
-        create_schedule(dataframe=df)
-        # Расчет коэффициента корреляции для каждой сферы
 
+        # Расчет коэффициента корреляции для каждой сферы
+        corr_coefficient_main(dataframe=df)
+        st.write("")
+
+        create_schedule_main(dataframe=df)
         st.write("")
         st.write("")
-        st.write("")
+
+        df_dict = extra_metrics(selected_extra)
+        if 'ВВП' in selected_extra:
+            vvp_dt = df_dict['ВВП']
+            corr_coefficient_extra(df_general=df, df_vvp=vvp_dt['dataframe'], extra_column=vvp_dt['column'])
+            create_schedule_vvp(df_general=df, df_vvp=vvp_dt['dataframe'], extra_column=vvp_dt['column'])
+
+        if 'ВВП на душу населения' in selected_extra:
+            vvp_dt_per = df_dict['ВВП на душу населения']
+            corr_coefficient_extra(df_general=df, df_vvp=vvp_dt_per['dataframe'], extra_column=vvp_dt_per['column'])
+            create_schedule_vvp(df_general=df, df_vvp=vvp_dt_per['dataframe'], extra_column=vvp_dt_per['column'])
+
+        if 'ВНП' in selected_extra:
+            df = df_dict['ВНП']
+            pass
+
+        if 'Уровень счастья' in selected_extra:
+            df = df_dict['Уровень счастья']
+            pass
+
+        if 'Уровень безработицы' in selected_extra:
+            df = df_dict['Уровень безработицы']
+            pass
+
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
 
     st.sidebar.title("About")
     st.sidebar.info(
